25f88ef2a1d5134bc18bcf4c1a4f68bb0aaedc9d
S a g h u <juanpablonotion@gmail.com>
2026-09-23 13:50:31 -0300

    Add files via upload

---
+++ Recuperacao_Somativa/rec01_producao.js
@@ -0,0 +1,4 @@
+const caixasPorHora = 75;
+const horasTrabalhadas = 8;
+const producaoTotal = caixasPorHora * horasTrabalhadas;
+console.log(`A máquina produz ${caixasPorHora} caixas por hora. Em um dia de ${horasTrabalhadas} horas de trabalho, o total produzido será de ${producaoTotal} caixas.`);
